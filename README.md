042519c1687ef892d2d959e2b3a3f3b905b07982
Jacob Meyers <jacobmeyers6@outlook.com>
2022-10-12 16:50:16 -0700

    generates password. WIP readme, not deployed yet

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# password-generator
-Generate strong and safe passwords with Javascript
+# Password Generator
+
+![](https://img.shields.io/badge/JavaScript-yellow.svg?style=flat-square)
+
+## Table of Contents
+* [Description](#description)
+* [Photos](#photos)
+* [License](#license)
+* [Deployed Page](#deployed-page)
+* [Contact](#contact)
+
+## Description
+My motivation behind making this project was to further my understanding of JavaScript. I built this project to practice with using arrays, loops, and strings. While the passwords generated might not be the most secure based on today's cybersecurity standards, they're completely (Math.random) random and made to the user's specifications.
+
+## Photos
+![The password generator is a white page with a text box for the generated password along with a button that says "Generate".](./assets/images/Screenshot%202022-10-12%20164856.png)
+
+## License
+Please refer to the LICENSE in the repo.
+
+## Deployed Page
+<a href="https://emiliatrentham.github.io/semantic-html-refactor/%22%3EDeployed Project</a>
+
+## Contact
+<a href="https://www.linkedin.com/in/emilia-trentham-987a59164/" >LinkedIn: Emilia Trentham</a>
+
+<a href="https://github.com/emiliatrentham%22%3EGithub: emiliatrentham</a>
--- assets/script.js
+++ assets/script.js
@@ -45,17 +45,29 @@ function generatePassword() {
   var includeSpecial = false;
   var userInput;
 
-  ///////// PROMPTS /////////
-  //ask user if they want a certain character set included in
-  //the randomizer, and validates the response. if they want
-  //that character type, include that array in the final
-  //password generation
 
-  //check y/n function
+  
+  ///////// HELPERS ////////
+  
+  //if input is Y/y, return true. if anything else, return false
   function checkYN(input) {
     if (input.toLowerCase() === 'y') { return true; }
     else { return false; }
   }
+
+  //returns a random element from a given array
+  function getRandElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+
+
+  ///////// PROMPTS /////////
+
+  //ask user if they want a certain character set included in
+  //the randomizer, and validates the response. if they want
+  //that character type, include that array in the final
+  //password generation
 
   //number of characters (8-128)
   userInput = prompt("How many characters? (8-128)");
@@ -66,7 +78,7 @@ function generatePassword() {
     for (var i = 0; i < numChars; i++) {
       password.push('');
     }
-    console.log(password);
+    // console.log(password);
   }
 
   else { return "invalid"; }
@@ -76,7 +88,7 @@ function generatePassword() {
   includeLower = checkYN(prompt("Include lowercase letters? y/n"));
   // console.log("includeLower: " + includeLower);
 
-  
+
   //uppercase?
   includeUpper = checkYN(prompt("Include uppercase letters? y/n"));
   // console.log("includeUpper: " + includeUpper);
@@ -91,12 +103,90 @@ function generatePassword() {
   includeSpecial = checkYN(prompt("Include special letters? y/n"));
   // console.log("includeSpecial: " + includeSpecial);
 
+
   //if none of them are true then the password is not generated
   if (!(includeUpper || includeLower) && !(includeNums || includeSpecial)) { return "invalid"; }
 
-  //////// GENERATE ////////
-  //
-  
 
+
+  //////// GENERATE ////////
+
+  //guarantees the presence of at least one of each requested character
+  //type and sets up array for getting random characters to fill the rest
+
+  var chars = [];
+
+  //did we want uppercase characters?
+  if (includeUpper) {
+    while (true) {
+      //gets a random index in the password
+      var i = Math.floor(Math.random() * password.length);
+      //is that space empty?
+      if (password[i] === '') {
+        //fill it with a random uppercase character and exit the loop
+        password[i] = getRandElement(uppercaseChars);
+        break;
+      }
+    }
+    //add uppercase characters to cumulative list of character options
+    chars = chars.concat(uppercaseChars);
+  }
+  
+  
+  if (includeLower) {
+    while (true) {
+      var i = Math.floor(Math.random() * password.length);
+      if (password[i] === '') {
+        password[i] = getRandElement(lowercaseChars);
+        break;
+      }
+    }
+    
+    chars = chars.concat(lowercaseChars);
+  }
+  
+  if (includeNums) {
+    while (true) {
+      var i = Math.floor(Math.random() * password.length);
+      if (password[i] === '') {
+        password[i] = getRandElement(numericChars);
+        break;
+      }
+    }
+    
+    chars = chars.concat(numericChars);
+  }
+  
+  if (includeSpecial) {
+    while (true) {
+      var i = Math.floor(Math.random() * password.length);
+      if (password[i] === '') {
+        password[i] = getRandElement(specialChars);
+        break;
+      }
+    }
+    
+    chars = chars.concat(specialChars);
+  }
+
+  // console.log(chars);
+  
+  //initialize string to return
+  var final = "";
+
+  //for each index in the password array
+  for (var i = 0; i < password.length; i++) {
+    //is the space at the iterator index empty?
+    if (password[i] === '') {
+      //fill it with a random character from our cumulative
+      //character options array and move to next index
+      password[i] = getRandElement(chars);
+    }
+
+    //add current character to the final string
+    final = final + password[i];
+  }
+
+  return final;
 
 }
